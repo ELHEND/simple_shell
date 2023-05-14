09a487de32e0a2118aa4dc9798e66c2227928baf
wafae10 <zwafae13@gmail.com>
2023-05-14 03:15:47 -0700

    changes done

--- HW_2shell.c
+++ HW_2shell.c
@@ -7,95 +7,93 @@
  */
 int main(void)
 {
-	char *line;
-	char **args;
-	int status;
-
-	do {
-		printf("$ ");
-		line = read_line();
-		args = split_line(line);
-		status = execute(args);
-
-		free(line);
-		free(args);
-	} while (status);
-
-	return (0);
+char *line;
+char **args;
+int status;
+do {
+printf("$ ");
+line = read_line();
+args = split_line(line);
+status = execute(args);
+free(line);
+free(args);
+} while (status);
+return (0);
 }
 /**
- * read_line - reads a line from stdin
+ * read_line - reads the input line from stdin.
  *
- * Return: pointer to the line.
+ * Return: a pointer to the line.
  */
 char *read_line(void)
 {
-	char *line = NULL;
-	ssize_t bufsize = 0;
-
-	getline(&line, &bufsize, stdin);
-	return (line);
+char *line = NULL;
+ssize_t bufsize = 0;
+getline(&line, &bufsize, stdin);
+return (line);
 }
 
 /**
- * split_line - splits a string into tokens
- * @line: pointer to the string
+ * split_line - splits the line into an array of tokens.
+ * @line: pointer to the string.
  *
  * Return: pointer to the array of tokens.
  */
 char **split_line(char *line)
 {
-	int bufsize = TOKEN_BUFSIZE, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char *));
-	char *token;
-
-	if (!tokens)
-		exit(EXIT_FAILURE);
-
-	token = strtok(line, TOKEN_DELIM);
-	while (token != NULL) {
-		tokens[position] = token;
-		position++;
-
-		if (position >= bufsize) {
-			bufsize += TOKEN_BUFSIZE;
-			tokens = realloc(tokens, bufsize * sizeof(char *));
-			if (!tokens)
-				exit(EXIT_FAILURE);
-		}
-
-		token = strtok(NULL, TOKEN_DELIM);
-	}
-	tokens[position] = NULL;
-	return (tokens);
+int bufsize = TOKEN_BUFSIZE, position = 0;
+char **tokens = malloc(bufsize * sizeof(char *));
+char *token;
+if (!tokens)
+exit(EXIT_FAILURE);
+token = strtok(line, TOKEN_DELIM);
+while (token != NULL)
+{
+tokens[position] = token;
+position++;
+if (position >= bufsize)
+{
+bufsize += TOKEN_BUFSIZE;
+tokens = realloc(tokens, bufsize *sizeof(char *));
+if (!tokens)
+exit(EXIT_FAILURE);
+}
+token = strtok(NULL, TOKEN_DELIM);
+}
+tokens[position] = NULL;
+return (tokens);
 }
 
 /**
- * execute - executes a command
- * @args: array of arguments
+ * execute - executes a command.
+ * @args: array of arguments.
  *
- * Return: 1 if the shell should continue running, 0 otherwise.
+ * Return: 1 if the shell must continue running, 0 otherwise.
  */
 int execute(char **args)
 {
-	pid_t pid;
-	int status;
-
-	if (args[0] == NULL)
-		return (1);
-
-	pid = fork();
-	if (pid == 0) {
-		if (execvp(args[0], args) == -1) {
-			perror("Error");
-		}
-		exit(EXIT_FAILURE);
-	} else if (pid < 0) {
-		perror("Error");
-	} else {
-		do {
-			waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
-	return (1);
+pid_t pid;
+int status;
+if (args[0] == NULL)
+return (1);
+pid = fork();
+if (pid == 0)
+{
+if (execvp(args[0], args) == -1)
+{
+perror("Error");
+}
+exit(EXIT_FAILURE);
+}
+else if (pid < 0)
+{
+perror("Error");
+}
+else
+{
+do {
+waitpid(pid, &status, WUNTRACED);
+} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+}
+return (1);
 }
