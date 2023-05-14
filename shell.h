88fdc0490658f125e0c3c18f9abdf049248ca21b
ELHEND <alhnd683@gmail.com>
2023-05-14 00:27:17 -0700

    xxxxx

--- shell.h
+++ shell.h
@@ -1,5 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
+#define MAX_CMD_LEN 100
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,6 +19,9 @@ void print_to_98(int n);
 /* HW_shell.c */
 char *read_line(void);
 char **parse_line(char *line);
+int execute_command(char **ar);
+/* HW_2shell.c */
+int read_command(char **cmd);
 int execute_command(char **args);
 
 #endif
