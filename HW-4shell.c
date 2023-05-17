48573735cd1365be119d8586468628e8dd356808
wafae10 <zwafae13@gmail.com>
2023-05-17 01:27:34 -0700

    up

---
+++ HW-4shell.c
@@ -0,0 +1,97 @@
+#include "shell.h"
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+    char input[1024];
+    char **args;
+    int status;
+
+    do {
+        printf("$ ");
+        fgets(input, sizeof(input), stdin);
+        args = parse_input(input);
+        status = execute_command(args);
+        free_args(args);
+    } while (status);
+
+    return (EXIT_SUCCESS);
+}
+
+/**
+ * execute_command - Executes a command from the user
+ * @args: An array of pointers to argument strings
+ *
+ * Return: 1 to continue running, 0 to exit
+ */
+int execute_command(char **args)
+{
+    if (args == NULL || args[0] == NULL)
+        return (1);
+
+    if (strcmp(args[0], "exit") == 0)
+        return (exit_shell());
+
+    /* TODO: Implement other commands here */
+
+    return (1);
+}
+
+/**
+ * parse_input - Parses the user's input into an array of arguments
+ * @input: The user's input string
+ *
+ * Return: An array of argument strings
+ */
+char **parse_input(char *input)
+{
+    int i = 0;
+    char **args = malloc(sizeof(char *) * 64);
+    char *token;
+
+    if (args == NULL)
+        return (NULL);
+
+    token = strtok(input, " \t\n");
+
+    while (token != NULL) {
+        args[i] = strdup(token);
+        i++;
+
+        token = strtok(NULL, " \t\n");
+    }
+
+    args[i] = NULL;
+
+    return (args);
+}
+
+/**
+ * free_args - Frees the memory used by an array of argument strings
+ * @args: The array of argument strings to free
+ */
+void free_args(char **args)
+{
+    int i;
+
+    for (i = 0; args && args[i]; i++) {
+        free(args[i]);
+    }
+
+    free(args);
+}
+
+/**
+ * exit_shell - Exits the shell
+ *
+ * Return: Always returns 0
+ */
+int exit_shell(void)
+{
+    printf("Exiting shell...\n");
+    return (0);
+}
