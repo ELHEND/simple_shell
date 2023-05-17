67309a3080132f05d43819b6df74681b15ab337e
wafae10 <zwafae13@gmail.com>
2023-05-17 06:00:36 -0700

    changes done

--- HW-4shell.c
+++ HW-4shell.c
@@ -1,44 +1,37 @@
 #include "shell.h"
-
 /**
- * main - The entry point of the program
+ * main - The entry point of the program of Simple shell 0.3 +
  *
- * Return: Always returns 0
+ * Return: 0
  */
 int main(void)
 {
-    char input[1024];
-    char **args;
-    int status;
-
-    do {
-        printf("$ ");
-        fgets(input, sizeof(input), stdin);
-        args = parse_input(input);
-        status = execute_command(args);
-        free_args(args);
-    } while (status);
-
-    return (EXIT_SUCCESS);
+char input[1024];
+char **args;
+int status;
+do {
+printf("$ ");
+fgets(input, sizeof(input), stdin);
+args = parse_input(input);
+status = execute_command(args);
+free_args(args);
+} while (status);
+return (EXIT_SUCCESS);
 }
 
 /**
- * execute_command - Executes a command from the user
- * @args: An array of pointers to argument strings
+ * execute_command - executes input command from the user.
+ * @args: An array of pointers to argument strings.
  *
- * Return: 1 to continue running, 0 to exit
+ * Return: 1 (if continue running), 0 (if exit)
  */
 int execute_command(char **args)
 {
-    if (args == NULL || args[0] == NULL)
-        return (1);
-
-    if (strcmp(args[0], "exit") == 0)
-        return (exit_shell());
-
-    /* TODO: Implement other commands here */
-
-    return (1);
+if (args == NULL || args[0] == NULL)
+return (1);
+if (strcmp(args[0], "exit") == 0)
+return (exit_shell());
+return (1);
 }
 
 /**
@@ -49,40 +42,33 @@ int execute_command(char **args)
  */
 char **parse_input(char *input)
 {
-    int i = 0;
-    char **args = malloc(sizeof(char *) * 64);
-    char *token;
-
-    if (args == NULL)
-        return (NULL);
-
-    token = strtok(input, " \t\n");
-
-    while (token != NULL) {
-        args[i] = strdup(token);
-        i++;
-
-        token = strtok(NULL, " \t\n");
-    }
-
-    args[i] = NULL;
-
-    return (args);
+int i = 0;
+char **args = malloc(sizeof(char *) * 64);
+char *token;
+if (args == NULL)
+return (NULL);
+token = strtok(input, " \t\n");
+while (token != NULL)
+{
+args[i] = strdup(token);
+i++;
+token = strtok(NULL, " \t\n");
+}
+args[i] = NULL;
+return (args);
 }
 
 /**
- * free_args - Frees the memory used by an array of argument strings
+ * free_args - frees the memory.
+ *
  * @args: The array of argument strings to free
  */
 void free_args(char **args)
 {
-    int i;
-
-    for (i = 0; args && args[i]; i++) {
-        free(args[i]);
-    }
-
-    free(args);
+int i;
+for (i = 0; args && args[i]; i++)
+free(args[i]);
+free(args);
 }
 
 /**
@@ -92,6 +78,6 @@ void free_args(char **args)
  */
 int exit_shell(void)
 {
-    printf("Exiting shell...\n");
-    return (0);
+printf("Exiting shell...\n");
+return (0);
 }
