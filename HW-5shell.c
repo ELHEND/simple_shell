88b41c0ca6bf4c2922555dca44b1482c1b6603aa
ELHEND <alhnd683@gmail.com>
2023-05-18 03:09:00 -0700

    cv

--- HW-5shell.c
+++ HW-5shell.c
@@ -1,14 +1,14 @@
-nclude "shell.h"
-
-extern char **environ;
-
+#include "shell.h"
 /**
  *  * print_environment - prints the current environment
- *   */
+ */
 void print_environment(void)
 {
 	char **env = environ;
-	while (*env != NULL) {
+
+	while (*env != NULL)
+
+	{
 		printf("%s\n", *env);
 		env++;
 	}
@@ -18,22 +18,28 @@ void print_environment(void)
  *  * main - simple shell program
  *   *
  *    * Return: 0 on success
- *     */
+ */
 int main(void)
 {
 	char command[100];
 
-	while (1) {
+	while (1)
+	{
 		printf("$ ");
 		fgets(command, sizeof(command), stdin);
 
-		if (strcmp(command, "env\n") == 0) {
+		if (strcmp(command, "env\n") == 0)
+		{
 			print_environment();
-		} else if (strcmp(command, "exit\n") == 0) {
+}
+
+else if (strcmp(command, "exit\n") == 0)
+
+{
 			break;
 		}
 	}
 
-	return 0;
+	return (0);
 }
 
