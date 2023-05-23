193141eb51fd24fe268892d41a9a5741edf1b43b
ELHEND <alhnd683@gmail.com>
2023-05-23 09:27:17 -0700

    ghj

--- HW_errors1.c
+++ HW_errors1.c
@@ -1,16 +1,18 @@
 #include "shell.h"
 
+
 /**
  * * HW_erratoi - convert a string to an integer.
  * * @s: the string to be converted.
  * *
  * * Return: 0 if no numbers in string, converted numbers otherwise.
  * * (-1) if error.
- * */
+ */
 int HW_erratoi(char *s)
 {
 	int m = 0;
 	unsigned long int result = 0;
+
 	if (*s == '+')
 		s++;
 	for (m = 0; s[m] != '\0'; m++)
@@ -34,7 +36,7 @@ int HW_erratoi(char *s)
  * *
  * * Return: 0 if no numbers in string, converted number otherwise
  * * (-1) if error
- * */
+ */
 void print_error(info_t *info, char *estr)
 {
 	HW_eputs(info->fname);
@@ -51,12 +53,13 @@ void print_error(info_t *info, char *estr)
  * * @yb: the filedescriptor to write.
  * *
  * * Return: number of characters printed.
- * */
+ */
 int print_d(int input, int yb)
 {
 	int (*__putchar)(char) = _putchar;
 	int m, count = 0;
 	unsigned int abs, current;
+
 	if (yb == STDERR_FILENO)
 		__putchar = _eputchar;
 	if (input < 0)
@@ -88,7 +91,8 @@ int print_d(int input, int yb)
  * * @flags: flags.
  * *
  * * Return: string.
- * */
+ */
+
 char *convert_number(long int num, int base, int flags)
 {
 	static char *array;
@@ -96,6 +100,7 @@ char *convert_number(long int num, int base, int flags)
 	char sign = 0;
 	char *ptr;
 	unsigned long o = num;
+
 	if (!(flags & CONVERT_UNSIGNED) && num < 0)
 	{
 		o = -num;
@@ -106,7 +111,7 @@ char *convert_number(long int num, int base, int flags)
 	*ptr = '\0';
 	do	{
 		*--ptr = array[n % base];
-		n/= base;
+		n /= base;
 	} while (o != 0);
 	if (sign)
 		*--ptr = sign;
@@ -117,11 +122,13 @@ char *convert_number(long int num, int base, int flags)
  * * @BUF: address of the string to modify
  * *
  * * Return: Always 0;
- * */
+ */
 void remove_comments(char *BUF)
 {
 	int m;
+
 	for (m = 0; BUF[m] != '\0'; m++)
+
 		if (BUF[m] == '#' && (!m || BUF[m - 1] == ' '))
 		{
 			BUF[m] = '\0';
