8343dc3c1d6536468615a9fce10cc7c5a071ed1a
ELHEND <alhnd683@gmail.com>
2023-05-24 07:28:51 -0700

    zsf

--- HW_execute.c
+++ HW_execute.c
@@ -1,43 +1,39 @@
 #include "shell.h"
 /**
-* execute_command - Executes a command using the function(execv)
-* @ar: Array of arguments for the command.
-*
-* Return: 1 (if success), 0 (if failure).
-*/
+ * * execute_command - Executes a command using the function(execv)
+ * * @ar: Array of arguments for the command.
+ * *
+ * * Return: 1 (if success), 0 (if failure).
+ */
 int execute_command(char **ar)
 {
-pid_t pid;
-int status;
-if (ar[0] == NULL)
-return (1);
-<<<<<<< HEAD
-if (strcmp(ar[0], "exit") == 0)
-return (exit_shell());
-return (1);
-}
-=======
->>>>>>> cfe34e1c7c62114b274e4ddc24f2848808c815a5
-pid = fork();
-if (pid == 0)
-{
-/* Child process */
-char *program_path = "/usr/bin/ls";
-if (execv(program_path, ar) == -1)
-perror("Error");
-exit(EXIT_FAILURE);
-}
-else if (pid < 0)
-{
-/* Error forking */
-perror("Error");
-}
-else
-{
-/* Parent process */
-do {
-waitpid(pid, &status, WUNTRACED);
-} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-}
-return (1);
+	pid_t pid;
+	int status;
+	if (ar[0] == NULL)
+		return (1);
+	if (strcmp(args[0], "exit") == 0)
+		return (exit_shell());
+	return (1);
+	pid = fork();
+	if (pid == 0)
+	{
+		/* Child process */
+		char *program_path = "/usr/bin/ls";
+		if (execv(program_path, ar) == -1)
+			perror("Error");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		/* Error forking */
+		perror("Error");
+	}
+	else
+	{
+		/* Parent process */
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1);
 }
