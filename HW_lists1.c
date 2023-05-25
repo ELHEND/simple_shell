977c160cd54d2a15ab400176ce2f15950082ae9d
ELHEND <alhnd683@gmail.com>
2023-05-25 01:41:12 -0700

    s

---
+++ HW_lists1.c
@@ -0,0 +1,117 @@
+#include "shell.h"
+/**
+ *  * list_len - determines length of linked list
+ *   * @h: pointer to first node
+ *    * Return: size of list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t m = 0;
+
+	while (h)
+	{
+		h = h->next;
+		m++;
+	}
+	return (m);
+}
+
+/**
+ *  * list_to_strings - returns an array of strings of the list->str
+ *   * @head: pointer to first node
+ *    *
+ *     * Return: array of strings
+ */
+char **list_to_strings(list_t *head)
+{
+	list_t *node = head;
+	size_t m = list_len(head), j;
+	char **strs;
+	char *str;
+
+	if (!head || !m)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (m + 1));
+	if (!strs)
+		return (NULL);
+	for (m = 0; node; node = node->next, m++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (j = 0; j < m; j++)
+				free(strs[j]);
+			free(strs);
+			return (NULL);
+		}
+
+		str = _strcpy(str, node->str);
+		strs[m] = str;
+	}
+	strs[m] = NULL;
+	return (strs);
+}
+
+
+/**
+ *  * print_list - prints all elements of a list_t linked list
+ *   * @h: pointer to first node
+ *    * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t m = 0;
+
+	while (h)
+	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		m++;
+	}
+	return (m);
+}
+
+/**
+ *  * node_starts_with - returns node whose string starts with prefix
+ *   * @node: pointer to list head
+ *    * @prefix: string to match
+ *     * @c: the next character after prefix to match
+ *      * Return: match node or null
+ */
+list_t *node_starts_with(list_t *node, char *prefix, char c)
+{
+	char *p = NULL;
+
+	while (node)
+	{
+		p = starts_with(node->str, prefix);
+		if (p && ((c == -1) || (*p == c)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ *  * get_node_index - gets the index of a node
+ *   * @head: pointer to list head
+ *    * @node: pointer to the node
+ *     * Return: index of node or -1
+ */
+ssize_t get_node_index(list_t *head, list_t *node)
+{
+	size_t m = 0;
+
+	while (head)
+	{
+		if (head == node)
+			return (m);
+		head = head->next;
+		m++;
+	}
+	return (-1);
+}
--- main.c
+++ main.c
@@ -1,24 +1,42 @@
 #include "shell.h"
-
 /**
- * main - Entry point of the program
- * Return: always 0 on success
+ *  * main - entry point
+ *   * @ac: arg count
+ *    * @av: arg vector
+ *     * Return: 0 on success, 1 on error
  */
-int main(void)
+int main(int ac, char **av)
 {
-char *line = NULL;
-char **ar = NULL;
-int status = 0;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-do {
-printf("#cisfun$ ");
-line = read_line();
-ar = parse_line(line);
-status = execute_command(ar);
-free(line);
-free(ar);
-} while (status);
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
 
-return (EXIT_SUCCESS);
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
-
