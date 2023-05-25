63c350178209c599a026d1e0ce2fed1f5a8cf38a
ELHEND <alhnd683@gmail.com>
2023-05-25 07:46:28 -0700

    b

--- HW_parser.c
+++ HW_parser.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 /**
- *  * is_cmd - determines if a file is an executable command
+ *  * is_cmd - Determines whether a file is an executable command.
  *   * @info: the info struct
  *    * @path: path to the file
  *     * Return: 1 if true, 0 otherwise
@@ -21,7 +21,8 @@ int is_cmd(info_t *info, char *path)
 }
 
 /**
- *  * dup_chars - duplicates characters
+ *  * dup_chars - Identifies duplicate characters.
+ *  *
  *   * @pathstr: the PATH string
  *    * @start: starting index
  *     * @stop: stopping index
@@ -40,7 +41,7 @@ char *dup_chars(char *pathstr, int start, int stop)
 }
 
 /**
- *  * find_path - finds this cmd in the PATH string
+ *  * find_path - Locates this command in the PATH string.
  *   * @info: the info struct
  *    * @pathstr: the PATH string
  *     * @cmd: the cmd to find
